94c852593587c0e5937fc9955a1c29858850c8a5
akeemlag <akeemifedayolag@gmail.com>
2021-07-06 14:00:00 +0100

    HyperlinkedModelSerializers

--- tutorial/quickstart/serializers.py
+++ tutorial/quickstart/serializers.py
@@ -5,3 +5,9 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
