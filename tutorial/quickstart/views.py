c683b955717df285c04d68e93cd027ec75270b87
akeemlag <akeemifedayolag@gmail.com>
2021-07-06 14:00:00 +0100

    docstring

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -2,4 +2,11 @@ from django.contrib.auth.models import User, Group
 from rest_framework  import viewsets
 from rest_framework import permissions
 
-# Create your views here.
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
+
+
+class UserViewSet(views.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited
+    """
+    
