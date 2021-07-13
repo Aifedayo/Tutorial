d1dac759e9412b13a9d0810bf5760bdce8c739f2
akeemlag <akeemifedayolag@gmail.com>
2021-07-13 14:00:00 +0100

    namespace

--- tutorial/quickstart/urls.py
+++ tutorial/quickstart/urls.py
@@ -3,3 +3,10 @@ from rest_framework import routers
 from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
